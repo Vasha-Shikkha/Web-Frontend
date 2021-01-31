7e8960e6ab6302fe3d4c48b6c81d822fadf30c95
Waqar-107 <waqar.hassan866@gmail.com>
2021-02-01 02:56:57 +0600

    animation aadded in the completed-tracker

--- src/app/components/QuestionNumber/styles.js
+++ src/app/components/QuestionNumber/styles.js
@@ -26,6 +26,7 @@ const styles = makeStyles((theme) => ({
 		borderRadius: 10,
 		background: theme.palette.colors.violetMedium,
 		position: "absolute",
+		transition: "all 0.5s cubic-bezier(0.645, 0.045, 0.355, 1) 0s",
 	},
 }));
 
