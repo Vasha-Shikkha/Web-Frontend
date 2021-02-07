3db08cb2f9db084c84fdae075aafc18d05e9fe8e
Waqar-107 <waqar.hassan866@gmail.com>
2021-02-07 16:54:08 +0600

    added font

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./app/assets/fonts/Montserrat-Regular.ttf";
 import "./app/styles/index.css";
 import App from "./app/App";
 import * as serviceWorker from "./serviceWorker";
