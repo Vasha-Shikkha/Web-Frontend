964b3a4b50a6cf5c350ce96a082116bf8279548e
Waqar-107 <waqar.hassan866@gmail.com>
2021-02-08 00:59:49 +0600

    exercise-list done

--- src/app/views/ExerciseHome/index.js
+++ src/app/views/ExerciseHome/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+import {Grid} from "@material-ui/core";
 import BackArrowButton from "../../components/BackArrowButton";
 import JumbledSentenceIcon from "../../assets/exercise/jumbledSentence.svg";
 import SentenceMatchingIcon from "../../assets/exercise/sentenceMatching.svg";
@@ -15,18 +16,21 @@ const Vocabulary = (props) => {
 			name: "Jumbled Sentence/Word",
 			image: JumbledSentenceIcon,
 			link: "/jumbled-sentence",
+			questionQuantity: 8,
 		},
 
 		{
 			name: "Sentence Matching",
 			image: SentenceMatchingIcon,
 			link: "/sentence-matching",
+			questionQuantity: 2,
 		},
 
 		{
 			name: "Fill in The Blanks",
 			image: FillInTheBlanksIcon,
 			link: "/fill-in-the-blanks",
+			questionQuantity: 2,
 		},
 	];
 
@@ -34,14 +38,22 @@ const Vocabulary = (props) => {
 		<div className={classes.root}>
 			<BackArrowButton link={props.location.state.from} />
 			<div className={classes.exerciseContainer}>
-				{exerciseTypes.map((obj, idx) => (
-					<div className={classes.box} key={idx}>
-						<div className={classes.imageContainer}>
-							<img src={obj.image} alt="" className={classes.boxImage} />
-						</div>
-						<div className={classes.title}>{obj.name}</div>
-					</div>
-				))}
+				<Grid container spacing={3}>
+					{exerciseTypes.map((obj, idx) => (
+						<Grid key={idx} item xs={12} sm={12} md={6} lg={6} xl={6}>
+							<Link to={obj.link} className={classes.box}>
+								<div className={classes.imageContainer}>
+									<img src={obj.image} alt="" className={classes.boxImage} />
+								</div>
+								<div className={classes.titleContainer}>
+									<div className={classes.title}>{obj.name}</div>
+									<div
+										className={classes.questionQuantity}>{`${obj.questionQuantity} Questions`}</div>
+								</div>
+							</Link>
+						</Grid>
+					))}
+				</Grid>
 			</div>
 		</div>
 	);
