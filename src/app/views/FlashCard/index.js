aad91a22b07130948795c615e86ab2416a5990c8
Waqar-107 <waqar.hassan866@gmail.com>
2021-02-14 02:35:13 +0600

    added btn

--- src/app/views/FlashCard/index.js
+++ src/app/views/FlashCard/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Button from "../../components/Button";
 import styles from "./styles";
 
 const FlashCard = (props) => {
 	const classes = styles();
-	return <div className={classes.root}>flash</div>;
+
+	const next = () => {};
+
+	return (
+		<div className={classes.root}>
+			<div className={classes.navContainer}></div>
+			<div className={classes.contentContainer}></div>
+			<div className={`${classes.btnContainer} ${classes.centered}`}>
+				<Button text="Next" onClick={() => next()} styles={classes.btn} />
+			</div>
+		</div>
+	);
 };
 
 export default FlashCard;
--- src/app/views/FlashCard/styles.js
+++ src/app/views/FlashCard/styles.js
@@ -1,7 +1,41 @@
 import {makeStyles} from "@material-ui/core";
 
 const styles = makeStyles((theme) => ({
-	root: {},
+	root: {
+		padding: "0 5% 0 5%",
+		background: "white",
+	},
+
+	navContainer: {
+		height: "15vh",
+		width: "100%",
+	},
+
+	contentContainer: {
+		height: "70vh",
+		width: "100%",
+	},
+
+	btnContainer: {
+		height: "15vh",
+		width: "100%",
+	},
+
+	btn: {
+		height: "50%",
+		width: "100%",
+		color: "white",
+		background: theme.palette.colors.violetMedium,
+		borderRadius: 10,
+	},
+
+	centered: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignContent: "center",
+		alignItems: "center",
+	},
 }));
 
 export default styles;
