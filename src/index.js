26d5b29616277997791f9490a97647c2dab16ee6
Waqar-107 <waqar.hassan866@gmail.com>
2021-02-16 22:38:25 +0600

    service worker disabled

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import "./app/assets/fonts/Montserrat-Regular.ttf";
 import "./app/styles/index.css";
 import App from "./app/App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
