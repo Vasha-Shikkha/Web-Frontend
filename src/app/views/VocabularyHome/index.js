3494a7bbe20202e03e984bbc3761ecebbbb535e8
Waqar-107 <waqar.hassan866@gmail.com>
2021-03-16 01:49:37 +0600

    updated the code according to backend update

--- src/app/views/VocabularyHome/index.js
+++ src/app/views/VocabularyHome/index.js
@@ -3,9 +3,9 @@ import {Redirect} from "react-router-dom";
 import {getCommunicativeTopics} from "../../axios/services/topics";
 
 import Loading from "../../components/Loading";
-import colors from "../../styles/colors";
-import Triangle from "../../components/Triangle";
-import Button from "../../components/Button";
+// import colors from "../../styles/colors";
+// import Triangle from "../../components/Triangle";
+// import Button from "../../components/Button";
 import BackArrowButton from "../../components/BackArrowButton";
 
 import {Grid} from "@material-ui/core";
@@ -17,38 +17,43 @@ import styles from "./styles";
 const VocabularyHome = () => {
 	const classes = styles();
 	const [level, setLevel] = useState(1);
-	const [showTooltip, setShowTooltip] = useState([]);
+	//const [showTooltip, setShowTooltip] = useState([]);
 	const [redirect, setRedirect] = useState(null);
 	const [topics, setTopic] = useState([]);
 	const [routingStates, setRoutingStates] = useState({});
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		getCommunicativeTopics((err, axios_data) => {
+		let params = {
+			level: level,
+			type: "communicative",
+		};
+
+		setLoading(true);
+		getCommunicativeTopics(params, (err, axios_data) => {
 			if (err) console.error(err);
 			else {
 				setTopic(axios_data);
 				setLoading(false);
-				setShowTooltip(topics.map(() => false));
+				//setShowTooltip(topics.map(() => false));
 			}
 		});
-		// eslint-disable-next-line
-	}, []);
+	}, [level]);
 
-	const tooltipToggler = (idx) => {
-		let arr = [...showTooltip];
-		if (arr[idx]) arr[idx] = false;
-		else {
-			arr = showTooltip.map(() => false);
-			arr[idx] = true;
-		}
+	// const tooltipToggler = (idx) => {
+	// 	let arr = [...showTooltip];
+	// 	if (arr[idx]) arr[idx] = false;
+	// 	else {
+	// 		arr = showTooltip.map(() => false);
+	// 		arr[idx] = true;
+	// 	}
 
-		setShowTooltip(arr);
-	};
+	// 	setShowTooltip(arr);
+	// };
 
-	const tutorialBtnClick = (idx) => {
-		setRedirect("/tutorial");
-	};
+	// const tutorialBtnClick = (idx) => {
+	// 	setRedirect("/tutorial");
+	// };
 
 	const exerciseBtnClick = (idx) => {
 		setRedirect("/exercise");
@@ -68,8 +73,6 @@ const VocabularyHome = () => {
 				}}
 			/>
 		);
-
-	if (loading) return <Loading />;
 
 	return (
 		<div className={classes.root}>
@@ -100,22 +103,30 @@ const VocabularyHome = () => {
 					))}
 				</div>
 
-				<div className={classes.gridroot}>
-					<Grid container spacing={3}>
-						{topics.map((obj, idx) => (
-							<Grid item key={idx} xs={4} sm={4} md={2} lg={2} xl={2}>
-								<div className={classes.taskBoxOuter} key={idx} onClick={() => tooltipToggler(idx)}>
-									<div className={`${classes.taskBoxInner} ${classes.centered}`}>
-										<div className={`${classes.taskImgContainer} ${classes.centered}`}>
-											<img
-												src={obj.image ? obj.image : DummyTopic}
-												alt=""
-												className={classes.topicImg}
-											/>
-										</div>
-									</div>
-									<div className={classes.title}>{obj.name}</div>
+				{loading ? (
+					<Loading container={classes.loadingContainer} />
+				) : (
+					<div className={classes.gridroot}>
+						<Grid container spacing={3}>
+							{topics.map((obj, idx) => (
+								<Grid item key={idx} xs={4} sm={4} md={2} lg={2} xl={2}>
 									<div
+										className={classes.taskBoxOuter}
+										key={idx}
+										onClick={() => exerciseBtnClick(idx)}
+										//onClick={() => tooltipToggler(idx)} : tooltip is disabled for now, comment out when tutorial is ready
+									>
+										<div className={`${classes.taskBoxInner} ${classes.centered}`}>
+											<div className={`${classes.taskImgContainer} ${classes.centered}`}>
+												<img
+													src={obj.image ? obj.image : DummyTopic}
+													alt=""
+													className={classes.topicImg}
+												/>
+											</div>
+										</div>
+										<div className={classes.title}>{obj.name}</div>
+										{/* <div
 										className={classes.tooltip}
 										style={{display: showTooltip[idx] ? "" : "none"}}>
 										<Triangle color={colors.violetDark} size={10} direction="up" />
@@ -131,12 +142,13 @@ const VocabularyHome = () => {
 												onClick={() => exerciseBtnClick(idx)}
 											/>
 										</div>
+									</div> */}
 									</div>
-								</div>
-							</Grid>
-						))}
-					</Grid>
-				</div>
+								</Grid>
+							))}
+						</Grid>
+					</div>
+				)}
 			</div>
 		</div>
 	);
