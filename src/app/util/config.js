2b90df19b419475d4c2f1095aa899cb2df2ba23b
Waqar-107 <waqar.hassan866@gmail.com>
2021-03-23 00:28:12 +0600

    config upd

--- src/app/util/config.js
+++ src/app/util/config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//BASE_API_URL: "http://127.0.0.1:5000",
-	BASE_API_URL: "http://webbackend-env.eba-murxqmni.ap-southeast-1.elasticbeanstalk.com"
+	BASE_API_URL: "http://webbackend-env.eba-murxqmni.ap-southeast-1.elasticbeanstalk.com",
 };
