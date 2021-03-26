4b718023c57d68302413c3b7575cae20ec2afcd9
Waqar-107 <waqar.hassan866@gmail.com>
2021-03-26 23:53:22 +0600

    config updated

--- src/app/util/config.js
+++ src/app/util/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	BASE_API_URL: "http://127.0.0.1:5000",
-	//BASE_API_URL: "https://9q81vs2n0e.execute-api.ap-southeast-1.amazonaws.com/dev",
+	// BASE_API_URL: "http://127.0.0.1:5000",
+	BASE_API_URL: "https://9q81vs2n0e.execute-api.ap-southeast-1.amazonaws.com/dev",
 };
