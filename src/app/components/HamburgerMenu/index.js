e97f3c1fb8911f147c5b14bc1dc18b04cd3b361a
Waqar-107 <waqar.hassan866@gmail.com>
2021-06-26 14:10:46 +0600

    logout implemented. closes #96

--- src/app/components/HamburgerMenu/index.js
+++ src/app/components/HamburgerMenu/index.js
@@ -1,15 +1,22 @@
 import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
+import {AuthConsumer} from "../../stateHandlers/authContext";
 
 import styles from "./styles";
 
-const HamburgerMenu = () => {
+const HamburgerMenu = (props) => {
 	const classes = styles();
 	const history = useHistory();
 	const [checked, setChecked] = useState(false);
 
 	const handleCheckBox = () => {
 		setChecked(!checked);
+	};
+
+	const handleLogout = () => {
+		props.logout(() => {
+			history.push("/auth");
+		});
 	};
 
 	return (
@@ -39,10 +46,16 @@ const HamburgerMenu = () => {
 				<div className={classes.link} onClick={() => history.push("/about")}>
 					About
 				</div>
-				<div className={classes.link}>Logout</div>
+				<div onClick={handleLogout} className={classes.link}>
+					Logout
+				</div>
 			</div>
 		</div>
 	);
 };
 
-export default HamburgerMenu;
+const ConsumerComponent = (props) => (
+	<AuthConsumer>{({logout}) => <HamburgerMenu {...props} logout={logout} />}</AuthConsumer>
+);
+
+export default ConsumerComponent;
--- src/app/components/Navbar/index.js
+++ src/app/components/Navbar/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import {useHistory} from "react-router-dom";
+import {AuthConsumer} from "../../stateHandlers/authContext";
 import HamburgerMenu from "../HamburgerMenu";
 
 import styles from "./styles";
 
-const Navbar = () => {
+const Navbar = (props) => {
 	const classes = styles();
 	const history = useHistory();
+
+	const handleLogout = () => {
+		props.logout(() => {
+			history.push("/auth");
+		});
+	};
 
 	return (
 		<div className={classes.root}>
@@ -27,7 +34,9 @@ const Navbar = () => {
 					<div className={classes.link} onClick={() => history.push("/about")}>
 						About
 					</div>
-					<div className={classes.link}>Logout</div>
+					<div onClick={handleLogout} className={classes.link}>
+						Logout
+					</div>
 				</div>
 				<div className={classes.mobileNav}>
 					<HamburgerMenu />
@@ -37,4 +46,8 @@ const Navbar = () => {
 	);
 };
 
-export default Navbar;
+const ConsumerComponent = (props) => (
+	<AuthConsumer>{({logout}) => <Navbar {...props} logout={logout} />}</AuthConsumer>
+);
+
+export default ConsumerComponent;
