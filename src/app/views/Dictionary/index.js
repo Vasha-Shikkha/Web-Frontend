c551aec5e6b815a944228020490ec59662fdb4da
Waqar-107 <waqar.hassan866@gmail.com>
2021-06-26 14:29:51 +0600

    [#95] - added navbar in the view

--- src/app/views/Dictionary/index.js
+++ src/app/views/Dictionary/index.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Loading from "../../components/Loading";
+import Navbar from "../../components/Navbar";
 import styles from "./styles";
 
 const Dictionary = () => {
 	const classes = styles();
 
-	return <div>dict</div>;
+	return (
+		<div className={classes.root}>
+			<div className={classes.navContainer}>
+				<Navbar />
+			</div>
+
+			<div className={classes.contentContainer}></div>
+		</div>
+	);
 };
 
 export default Dictionary;
--- src/app/views/Dictionary/styles.js
+++ src/app/views/Dictionary/styles.js
@@ -1,7 +1,20 @@
 import {makeStyles} from "@material-ui/core";
 
 const styles = makeStyles((theme) => ({
-	root: {},
+	root: {
+		width: "100%",
+	},
+
+	navContainer: {
+		height: 70,
+		width: "100%",
+		position: "fixed",
+		zIndex: 2,
+	},
+
+	contentContainer: {
+		padding: "100px 5% 5% 5%",
+	},
 }));
 
 export default styles;
