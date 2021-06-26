b0422bf42ff75bae80987d75ae6f222bdab7d860
Waqar-107 <waqar.hassan866@gmail.com>
2021-06-26 15:18:08 +0600

    [#95] - search-bar added

--- src/app/views/Dictionary/index.js
+++ src/app/views/Dictionary/index.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import Loading from "../../components/Loading";
 import Navbar from "../../components/Navbar";
+import {getWordMeaning} from "../../axios/services/dictionary";
+import SearchIcon from "@material-ui/icons/Search";
 import styles from "./styles";
 
 const Dictionary = () => {
 	const classes = styles();
+	const [dictionarySearch, setDictionarySearch] = useState("");
+	const [searchRes, setSearchRes] = useState({});
+	const [loading, setLoading] = useState(false);
+
+	const searchWord = () => {
+		if (!dictionarySearch || dictionarySearch.length === 0) return;
+
+		setLoading(true);
+		getWordMeaning(dictionarySearch, (err, axios_data) => {
+			console.log(err, axios_data);
+			if (!err) {
+				setSearchRes(axios_data);
+			} else {
+				setSearchRes({word: "word not found"});
+			}
+
+			setLoading(false);
+		});
+	};
+
+	const handleKeyPress = (event) => {
+		if (event.key === "Enter") {
+			searchWord();
+		}
+	};
 
 	return (
 		<div className={classes.root}>
@@ -12,7 +39,25 @@ const Dictionary = () => {
 				<Navbar />
 			</div>
 
-			<div className={classes.contentContainer}></div>
+			<div className={classes.contentContainer}>
+				<div className={classes.searchContainer}>
+					<SearchIcon
+						className={classes.menuBtn}
+						onClick={() => {
+							searchWord();
+						}}
+					/>
+					<input
+						type="text"
+						name="dictionaryText"
+						placeholder="Search Meaning"
+						autoFocus={false}
+						className={classes.input}
+						onKeyPress={handleKeyPress}
+						onChange={(e) => setDictionarySearch(e.target.value)}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/app/views/Dictionary/styles.js
+++ src/app/views/Dictionary/styles.js
@@ -15,6 +15,42 @@ const styles = makeStyles((theme) => ({
 	contentContainer: {
 		padding: "100px 5% 5% 5%",
 	},
+
+	searchContainer: {
+		width: "100%",
+		height: 40,
+		borderRadius: 20,
+		padding: 5,
+
+		border: `2px solid ${theme.palette.colors.violetMedium}`,
+
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "flex-start",
+		alignContent: "center",
+		alignItems: "center",
+	},
+
+	input: {
+		width: "75%",
+		height: "100%",
+		border: "0px solid transparent",
+		color: theme.palette.colors.violetMedium,
+
+		"&:focus": {
+			outline: "none",
+		},
+
+		"&::placeholder": {
+			color: theme.palette.colors.violetMedium,
+		},
+	},
+
+	menuBtn: {
+		color: theme.palette.colors.primary,
+		fontSize: 30,
+		cursor: "pointer",
+	},
 }));
 
 export default styles;
