fe79e1ce8d03a7790f4550d64b2533dab8ea35f8
Waqar-107 <waqar.hassan866@gmail.com>
2021-10-17 02:13:50 +0600

    image attributes added

--- src/app/views/Tutorial/index.js
+++ src/app/views/Tutorial/index.js
@@ -2,16 +2,24 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import NotesLayout from "../../layouts/notesLayout";
 import remarkGfm from "remark-gfm";
-import styles from "./styles";
+import "./tutorialStyles.css";
+
+const Image = (props) => {
+	return <img {...props} style={{width: "100px"}} />;
+};
 
 const Tutorial = (props) => {
-	const classes = styles();
 	const {taskDetail} = props.location.state;
 
 	return (
 		<NotesLayout>
-			<div className={classes.root}>
-				<ReactMarkdown children={taskDetail.instruction} remarkPlugins={[remarkGfm]} />
+			<div className="root">
+				<ReactMarkdown
+					skipHtml={true}
+					children={taskDetail.instruction}
+					remarkPlugins={[remarkGfm]}
+					renderers={{image: Image}}
+				/>
 			</div>
 		</NotesLayout>
 	);
